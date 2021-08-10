f3fc4a2e35bcb1c9ba6f73f086cd5c074384f89f
Shivam Soni <78500235+vermashivam217@users.noreply.github.com>
2021-08-10 14:24:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,87 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+
+  state = {
+    users: null,
+    total: null,
+    per_page: null,
+    current_page: 1
+  }
+
+  componentDidMount() {
+    this.makeHttpRequestWithPage(1);
+  }
 
 
-const Pagination = () => {
-  const [users, setUsers] = useState([]);
-  const data = async () => {
-    const res = await fetch("https://reqres.in/api/users?page=2");
-    const json = await res.json();
-    setUsers(json.data);
-  };
-  useEffect(() => {
-    data();
-  }, []);
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <div className="flex">
-        {users.length &&
-          users.map((user) => {
-            return (
-              <div className="card">
-                <div key={user.id}>
-                  <p>
-                    <h4>{user.first_name} {user.last_name}</h4>
-                  </p>
-                  <p>{user.email}</p>
-                  <img key={user.avatar} src={user.avatar} />
-                </div>
-              </div>
-            );
-          })}
+  makeHttpRequestWithPage = async pageNumber => {
+    const response = await fetch(`https://reqres.in/api/users?page=${pageNumber}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    this.setState({
+      users: data.data,
+      total: data.total,
+      per_page: data.per_page,
+      current_page: data.page
+    });
+  }
+
+
+  render() {
+
+    let users, renderPageNumbers;
+
+    if (this.state.users !== null) {
+      users = this.state.users.map(user => (
+        <div className="card">
+          <div key={user.id}>
+            <img key={user.avatar} src={user.avatar} />
+            <p>
+              <h4>{user.first_name} {user.last_name}</h4>
+            </p>
+            <p>{user.email}</p>
+          </div>
+        </div>
+
+      ));
+    }
+
+    const pageNumbers = [];
+    if (this.state.total !== null) {
+      for (let i = 1; i <= Math.ceil(this.state.total / this.state.per_page); i++) {
+        pageNumbers.push(i);
+      }
+
+
+      renderPageNumbers = pageNumbers.map(number => {
+        let classes = this.state.current_page === number;
+
+        return (
+          <button key={number} className={classes} onClick={() => this.makeHttpRequestWithPage(number)}>{number}</button>
+        );
+      });
+    }
+    return (
+
+
+      <div>
+
+        {users}
+
+        <div class='fixx'>
+          <span onClick={() => this.makeHttpRequestWithPage(1)}>&laquo;</span>
+          {renderPageNumbers}
+          <span onClick={() => this.makeHttpRequestWithPage(2)}>&raquo;</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Pagination;
+export default App;
